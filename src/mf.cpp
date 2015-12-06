f6a5f291550426dbb0864a1da01672b4ca2a4d65
yixi1992 <yixi1992@gmail.com>
2015-12-06 01:55:34 -0500

    fixed mf.cpp, running well with data/train_corpus.txt data/test_corpus.txt

--- src/mf.cpp
+++ src/mf.cpp
@@ -4,7 +4,7 @@ F = acc_error + doc_reg + word_reg
 	= sum_{(doc_id, word_id, cnt):train_vec} (cnt - D(doc_id)*W(word_id))^2 + 0.5*lambda*(|D|^2 + |W|^2)
 
 The program should take two data files i.e. training data and testing data.
-The input data is in the format of 
+The input data is in the forvector<vector<double> > of 
 total_word_in_doc1 w_a(1,1):cnt_a(1,1) w_a(1,2):cnt_a(1,2) ... w_a(1,n1):cnt_a(1,n1)
 total_word_in_doc2 w_a(2,1):cnt_a(2,1) w_a(2,2):cnt_a(2,2) ... w_a(2,n2):cnt_a(2,n2)
 ...
@@ -15,11 +15,11 @@ where total_word_in_dock = \sum_i cnt_a(k,i)
 The program will print to the screen the training loss and accuracy on testing data in each epoch and batch. 
 
 Usage:
-g++ mf.cpp -o mf
+g++ src/mf.cpp -o mf
 ./mf data/train_corpus.txt data/test_corpus.txt
 
 Author: Xi Yi (yixi1992@gmail.com)
-Date: 11/05/2015
+Date: 12/05/2015
 */
 
 #include <stdio.h> // fscanf
@@ -32,7 +32,7 @@ Date: 11/05/2015
 #include <math.h> //sqrt
 using namespace std;
 
-#define RANDOM "random"
+#define RANDOM -1
 
 /*
 	Triplet structure where each triplet is {doc_id, word_id, cnt} 
@@ -43,186 +43,116 @@ struct Triplet{
 	Triplet(int dd, int ww, double cc):doc_id(dd), word_id(ww), cnt(cc){}
 };
 
-/*
-	Matrix structure.
-*/
-class Mat{
-	private:
-	
-	int n, m;
-	double **arr;
-	
-	/*
-		Generator who generates initial values for Mat elements.
-	*/
-	double ValueGenerator(string default_val){
-		double val = 0;
-		if (default_val==RANDOM){ // if default_val is random, draw a random number.
-			val = 0.1 * ((double)rand()) / RAND_MAX;
-		} else { // otherwise, default_val is a double value.
-			val = std::strtod(default_val.c_str(), NULL);
-		}
-		return val;
-	}
-	
-	/*
-		Convert double to string.
-	*/
-	string ToString(double d){
-		ostringstream strs;
-		strs << d;
-		return strs.str();
-	}
-	
-	
-	/*
-		Until c++11, constructor cannot call another constructor so I created this InitMat function to do initialization.
-	*/
-	void InitMat(int nn, int mm, string default_val=RANDOM) {
-		n = nn; m = mm;
-		arr = new double*[n];
-		for (int i=0; i<n; i++) {
-			arr[i] = new double[m];
-			for (int j=0; j<m; j++)
-				arr[i][j] = ValueGenerator(default_val);
-		}
-	}
-	
-	
-	public:
-	/*
-		Constructor for constructing a nn*mm matrix with default_val (double).
-	*/
-	Mat(int nn, int mm, double default_val){
-		InitMat(nn, mm, ToString(default_val));
-	}
-	
-	
-	/*
-		Constructor for constructing a nn*mm matrix with default_val.
-		default_val: a string indicating default values of the matrix which is either RANDOM or any double value, e.g. "0.5"
-	*/
-	Mat(int nn=0, int mm=0, string default_val = RANDOM) {
-		InitMat(nn, mm, default_val);
-	}
-	
-	/*
-		Copy constructor. Deep copy.
-	*/
-	Mat(const Mat &cp){
-		InitMat(cp.get_n(),cp.get_m());
-		for (int i=0; i<n; i++)
-			for (int j=0; j<m; j++)
-				arr[i][j] = cp.get(i,j);
-	}
-	
-	int get_n() const {
-		return n;
-	}
-	
-	int get_m() const {
-		return m;
-	}
-	
-	double get(int i, int j) const {
-		if (i>=n || i<0 || j<0 || j>=m) {
-			throw invalid_argument("Index out of Bound when using get(i,j) for Mat object.");
-		}
-		return arr[i][j];
-	}
-	
-	/*
-		This class (Proxy) is to enable client use two dimensional subscripts. i.e. Mat a=Mat(1,1,0); a[0][0]+=2;
-	*/
-	class Proxy {
-		public:
-			Proxy(double* _array) : _array(_array) { }
-
-			double & operator[](int index) {
-				return _array[index];
-			}
-		private:
-			double* _array;
-	};
-	
-	Proxy & operator[](int i){
-		if (i>=n || i<0) {
-			throw invalid_argument("Index out of Bound when using [] for Mat object.");
-		}
-		return *(new Proxy(arr[i]));
-	}
-	
-	
-	/*
-		Add two matrix elementwise. Throw exception if their dimensions don't match.
-	*/
-	Mat operator +(const Mat& rhs){
-		if (rhs.get_n()!=n || rhs.get_m()!=m){
-			throw invalid_argument("Matrix dimensions are not consistent when trying '+' two matrices");
-			return *this;
-		}
-		Mat res = Mat(*this);
-		for (int i=0; i<res.get_n(); i++){
-			for (int j=0; j<res.get_m(); j++)
-				res[i][j] += rhs.get(i,j);
-		}
-		return res;
-	}
-	
-	/*
-		Substraction. Throw exception if their dimensions don't match.
-	*/
-	Mat operator -(const Mat& rhs){
-		if (rhs.get_n()!=n || rhs.get_m()!=m){
-			throw invalid_argument("Matrix dimensions are not consistent when trying '+' two matrices");
-			return Mat();
-		}
-		Mat res = Mat(*this);
-		for (int i=0; i<res.get_n(); i++){
-			for (int j=0; j<res.get_m(); j++)
-				res[i][j] -= rhs.get(i, j);
-		}
-		return res;
-	}
-	
-	/*
-		Multiply by a number.
-	*/
-	Mat operator *(const double& mult){
-		Mat res = Mat(*this);
-		for (int i=0; i<res.get_n(); i++){
-			for (int j=0; j<res.get_m(); j++)
-				res[i][j] *= mult;
-		}
-		return res;
-	}
-	
-	/*
-		Return a matrix which is the elementwise square.
-	*/
-	Mat Sqr(){
-		Mat res = Mat(*this);
-		for (int i=0; i<res.get_n(); i++)
-			for (int j=0; j<res.get_m(); j++)
-				res[i][j] = res[i][j]*res[i][j];
-		return res;
-	}
-	
-	/*
-		Return the sum of all elements in the matrix.
-	*/
-	double Sum(){
-		double s;
-		for (int i=0; i<n; i++)
-			for (int j=0; j<m; j++)
-				s += arr[i][j];
-		return s;
-	}
-	
-};
-
 vector<Triplet> train_vec, probe_vec;
 vector<double> err_train, err_valid;
+
+
+vector<vector<double> > NewArray(int n, int m, int default_val){	
+	vector<vector<double> > a(n, vector<double>(m));
+	if (default_val==RANDOM){
+		for (int i=0; i<a.size(); i++)
+			for (int j=0; j<a[0].size(); j++)
+				a[i][j] = 0.1 * ((double)rand()) / RAND_MAX;
+	}
+	return a;
+	
+}
+
+/*
+	Add two matrix elementwise. Throw exception if their dimensions don't match.
+*/
+void Add(vector<vector<double> > & res, const vector<vector<double> > lhs, const vector<vector<double> > rhs){
+	if (rhs.size()!=lhs.size() || rhs[0].size()!=lhs[0].size()){
+		throw invalid_argument("matrix dimensions are not consistent when trying add two matrices");
+		return;
+	}
+	res.clear();
+	res = NewArray(lhs.size(), lhs[0].size(), 0);
+	for (int i=0; i<lhs.size(); i++){
+		for (int j=0; j<lhs[0].size(); j++)
+			res[i][j] = lhs[i][j]+rhs[i][j];
+	}
+}
+
+/*
+	Substraction. Throw exception if their dimensions don't match.
+*/
+void Minus(vector<vector<double> > & res, const vector<vector<double> > lhs, const vector<vector<double> > rhs){
+	if (rhs.size()!=lhs.size() || rhs[0].size()!=lhs[0].size()){
+		throw invalid_argument("matrix dimensions are not consistent when trying add two matrices");
+		return;
+	}
+	res.clear();
+	res = NewArray(lhs.size(), lhs[0].size(), 0);
+	for (int i=0; i<lhs.size(); i++){
+		for (int j=0; j<lhs[0].size(); j++)
+			res[i][j] = lhs[i][j]-rhs[i][j];
+	}
+}
+
+/*
+	Multiply by a number.
+*/
+void Scale(vector<vector<double> > & res, const vector<vector<double> > lhs, const double& mult){
+	res.clear();
+	res = NewArray(lhs.size(), lhs[0].size(), 0);
+	for (int i=0; i<lhs.size(); i++){
+		for (int j=0; j<lhs[0].size(); j++)
+			res[i][j] = lhs[i][j]*mult;
+	}
+}
+
+
+/*
+	Return the square of x.
+*/
+template <typename Type>
+Type sqr(Type x)
+{
+   return x * x;
+}
+
+
+/*
+	Elementwise square the matrix.
+*/
+vector<vector<double> > Sqr(const vector<vector<double> > & arr){
+	vector<vector<double> > res = NewArray(arr.size(), arr[0].size(), 0);
+	for (int i=0; i<arr.size(); i++)
+		for (int j=0; j<arr[0].size(); j++)
+			res[i][j] = sqr(arr[i][j]);
+	return res;
+}
+
+/*
+	Return the sum of all elements in the matrix.
+*/
+double Sum(const vector<vector<double> > &arr){
+	double s;
+	for (int i=0; i<arr.size(); i++)
+		for (int j=0; j<arr[0].size(); j++)
+			s += arr[i][j];
+	return s;
+}
+
+void reset(vector<vector<double> > &arr){
+	for (int i=0; i<arr.size(); i++)
+		for (int j=0; j<arr[0].size(); j++)
+			arr[i][j] = 0;
+}
+
+/*
+	Sum up the 'cnt' value in a vector of Triplet.
+	data_vec: the vector of Triplets to be summed up.
+*/
+double sum_cnt(vector<Triplet> &data_vec){
+	double sum = 0;
+	for (int i=0; i<data_vec.size(); i++){
+		sum += data_vec[i].cnt;
+	}
+	return sum;
+}
+
 
 /*
 	Load the data from data_file and save in data_vec as triplet. Indices start from 0 (NOT from 1).
@@ -236,7 +166,6 @@ void load_data(string data_file, vector<Triplet> &data_vec, int &num_d, int &num
 	int n, doc_id=cur_doc_id;
 	fscanf(pFile, "%d", &n);
 	while (fscanf(pFile, "%d", &n)>0){
-		printf("doc_id=%d n=%d\n",doc_id,n);
 		while (n>0){	
 			int word_id, cnt;
 			fscanf(pFile, "%d:%d", &word_id, &cnt);
@@ -252,26 +181,6 @@ void load_data(string data_file, vector<Triplet> &data_vec, int &num_d, int &num
 	fclose(pFile);
 }
 
-/*
-	Sum up the 'cnt' value in a vector of Triplet.
-	data_vec: the vector of Triplets to be summed up.
-*/
-double sum_cnt(vector<Triplet> &data_vec){
-	double sum = 0;
-	for (int i=0; i<data_vec.size(); i++){
-		sum += data_vec[i].cnt;
-	}
-	return sum;
-}
-
-/*
-	Return the square of x.
-*/
-template <typename Type>
-Type sqr(Type x)
-{
-   return x * x;
-}
 
 /*
 	Objective function value calculator:
@@ -281,17 +190,16 @@ Type sqr(Type x)
 		F = acc_error + doc_reg + word_reg 
 			= sum_{(doc_id, word_id, cnt):train_vec} (cnt - D(doc_id)*W(word_id))^2 + 0.5*lambda*(|D|^2 + |W|^2)
 */
-double CalcObj(const Mat &D, const Mat &W, const vector<Triplet> &train_vec, const int &begin_idx, const int &end_idx, const double &lambda, const double &mean_cnt, Mat &error, Mat &pred_out){
+double CalcObj(const vector<vector<double> > &D, const vector<vector<double> > &W, const vector<Triplet> &train_vec, const int &begin_idx, const int &end_idx, const double &lambda, const double &mean_cnt, vector<vector<double> > &error, vector<vector<double> > &pred_out){
 	double acc_error = 0.0, doc_reg = 0.0, word_reg = 0.0;
-	error = Mat(end_idx-begin_idx+1, 1, 0); pred_out = Mat(end_idx-begin_idx+1, 1, 0);
 	for (int i = begin_idx; i <= end_idx; i++){
 		int doc_id = train_vec[i].doc_id, word_id = train_vec[i].word_id;
 		double cnt = train_vec[i].cnt;
 		
 		// calculate the prediction: inner product of D(doc_id), W(word_id)
 		double predict = 0.0;
-		for (int j=0; j < D.get_m(); j++){
-			double dd = D.get(doc_id, j), ww = W.get(word_id, j);
+		for (int j=0; j < D[0].size(); j++){
+			double dd = D[doc_id][j], ww = W[word_id][j];
 			predict += (dd*ww);
 			doc_reg += sqr(dd);
 			word_reg += sqr(ww);
@@ -330,9 +238,9 @@ int main(int argc, char **argv){
 	if (!ParseMainArgs(argc, argv, train_file, probe_file)) return 0;
 	
 	
-	double epsilon = 0.5; // Learning rate
-	double lambda = 0.01; // Regularization parameter 
-	double momentum = 0.8;
+	double epsilon = 5; // Learning rate
+	double lambda = 1; // Regularization parameter 
+	double momentum = 0.9;
 	
 	
 	int epoch = 0;
@@ -348,7 +256,6 @@ int main(int argc, char **argv){
 	load_data(train_file, all_data_vec, tmp_num_d, num_w, cur_doc_id); // Triplets: {doc_id, word_id, cnt} 
 	load_data(probe_file, all_data_vec, tmp_num_d, num_w, cur_doc_id);
 	num_d = cur_doc_id;
-	printf("num_d=%d\n",num_d);
 	
 	for (int i=0; i<all_data_vec.size(); i++){
 		if (rand()%100<90) train_vec.push_back(all_data_vec[i]);
@@ -362,13 +269,15 @@ int main(int argc, char **argv){
 	double mean_cnt = sum_cnt(train_vec)/pairs_tr;
 	
 	int numbatches = 10; // Number of batches  
-	int N = (pairs_tr-1)/numbatches+1; // number training triplets per batch 
+	int NN = (pairs_tr-1)/numbatches+1; // number training triplets per batch 
 	
-	Mat D = Mat(num_d, num_feat); // Doc feature vectors
-	Mat W = Mat(num_w, num_feat); // Word feature vecators
-	Mat D_inc = Mat(num_d, num_feat, 0);
-	Mat W_inc = Mat(num_w, num_feat, 0);
-	Mat error, pred_out;
+	vector<vector<double> > D = NewArray(num_d, num_feat, RANDOM); // Doc feature vectors
+	vector<vector<double> > W = NewArray(num_w, num_feat, 0);// Word feature vecators
+	vector<vector<double> > D_inc = NewArray(num_d, num_feat, 0), W_inc = NewArray(num_w, num_feat, 0);
+	vector<vector<double> > error = NewArray(NN, 1, 0), pred_out = NewArray(NN, 1, 0), test_error = NewArray(pairs_pr, 1, 0), test_pred_out = NewArray(pairs_pr, 1, 0);
+	
+	vector<vector<double> > Ix_D = NewArray(NN, num_feat, 0), Ix_W = NewArray(NN, num_feat, 0); // gradient w.r.t. training sample
+	vector<vector<double> > d_D = NewArray(num_d, num_feat, 0), d_W = NewArray(num_w, num_feat, 0);
 
 	double F = 0.0;
 	
@@ -378,17 +287,14 @@ int main(int argc, char **argv){
 		
 		for (int batch = 1; batch <= numbatches; batch++ ){
 		
-			int begin_idx = (batch-1)*N, end_idx = min(batch*N-1, pairs_tr-1), N = end_idx - begin_idx +1;
-			printf("epoch %4i batch %4i begin_idx %4i end_idx %4i\n", epoch, batch, begin_idx, end_idx);
-
+			int begin_idx = (batch-1)*NN, end_idx = min(batch*NN-1, pairs_tr-1), N = end_idx - begin_idx +1;
+			
 			//%%%%%%%%%%%%%% Compute Predictions %%%%%%%%%%%%%%%%%
 			F = CalcObj(D, W, train_vec, begin_idx, end_idx, lambda, mean_cnt, error, pred_out);
 			
-
 			//%%%%%%%%%%%%%% Compute Gradients %%%%%%%%%%%%%%%%%%%
-			Mat Ix_D = Mat(N, num_feat, 0), Ix_W = Mat(N, num_feat, 0);
 			for (int i = begin_idx; i <= end_idx; i++){
-				int doc_id = train_vec[i].doc_id, word_id = train_vec[i].word_id;				
+				int doc_id = train_vec[i].doc_id, word_id = train_vec[i].word_id;	
 				for (int j=0; j < num_feat; j++){
 					double dd = D[doc_id][j], ww = W[word_id][j];
 					Ix_D[i - begin_idx][j] = error[i-begin_idx][0] *2* ww + lambda*dd;
@@ -396,9 +302,8 @@ int main(int argc, char **argv){
 				}
 			}
 			
-			Mat d_D = Mat(num_d, num_feat, 0);
-			Mat d_W = Mat(num_w, num_feat, 0);
-
+			reset(d_D);
+			reset(d_W);
 			for (int i = begin_idx; i <= end_idx; i++){
 				int doc_id = train_vec[i].doc_id, word_id = train_vec[i].word_id;
 				for (int j=0; j<num_feat; j++){
@@ -409,11 +314,19 @@ int main(int argc, char **argv){
 
 			//%%%% Update doc and word features %%%%%%%%%%%
 
-			D_inc = D_inc*momentum + d_D*(epsilon/N);
-			D =  D - D_inc;
+			Scale(D_inc, D_inc, momentum);
+			Scale(d_D, d_D, epsilon/N);
+			Add(D_inc, D_inc, d_D);
+			//D_inc = D_inc*momentum + d_D*(epsilon/N);
+			Minus(D, D, D_inc);
+			//D =  D - D_inc;
 
-			W_inc = W_inc*momentum + d_W*(epsilon/N);
-			W =  W - W_inc;
+			Scale(W_inc, W_inc, momentum);
+			Scale(d_W, d_W, epsilon/N);
+			Add(W_inc, W_inc, d_W);
+			//W_inc = W_inc*momentum + d_W*(epsilon/N);
+			Minus(W, W, W_inc);
+			//W =  W - W_inc;
 		
 			//%%%%%%%%%%%%%% Compute Predictions after Parameter Updates %%%%%%%%%%%%%%%%%
 			F = CalcObj(D, W, train_vec, begin_idx, end_idx, lambda, mean_cnt, error, pred_out);
@@ -421,9 +334,9 @@ int main(int argc, char **argv){
 
 			//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 			//%%% Compute predictions on the validation set %%%%%%%%%%%%%%%%%%%%%% 
-			F = CalcObj(D, W, probe_vec, 0, pairs_pr-1, lambda, mean_cnt, error, pred_out);
-			err_valid.push_back(sqrt(error.Sqr().Sum()/pairs_pr));
-			printf("epoch %4i batch %4i Training RMSE %6.4f  Test RMSE %6.4f  \n", epoch, batch, err_train[epoch], err_valid[epoch]);
+			F = CalcObj(D, W, probe_vec, 0, pairs_pr-1, lambda, mean_cnt, test_error, test_pred_out);
+			err_valid.push_back(sqrt(Sum(Sqr(test_error))/pairs_pr));
+			printf("epoch %4i batch %4i Training RMSE %6.4f  Test RMSE %6.4f  \n", epoch, batch, err_train.back(), err_valid.back());
 		}
 		//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 	}
